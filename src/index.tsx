4ab9d536f63c6f31be9fd03f070c4c38ca6d6cfb
Alvaro Batista <alvarobatista87@outlook.com>
2023-05-21 09:28:53 -0300

    :hammer: Refactor: add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Menu from './pages/Menu';
 import './index.css';
 import 'normalize.css';
-import Start from 'pages/Start';
+import Router from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Start />
+    <Router />
   </React.StrictMode>
 );
 
