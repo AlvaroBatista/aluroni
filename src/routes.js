7b9707d42878cfc2e3214dea5a30719a428973ba
Alvaro Batista <alvarobatista87@outlook.com>
2023-05-22 20:11:02 -0300

    :hammer: Refactor: create Header component

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,18 @@ import NavBar from 'components/NavBar';
 import Menu from 'pages/Menu';
 import Start from 'pages/Start';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './Route.module.scss';
+import Header from 'components/Header';
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <NavBar />
-        <header className={styles.header}>
-          <div className={styles.header__text}>
-          A casa do código e da massa
-          </div>
-        </header>
         <Routes>
-          <Route path='/' element={<Start />} />
-          <Route path='/cardapio' element={<Menu />} />
+          <Route path='/' element ={<Header />}>
+            <Route index element={<Start />} />
+            <Route path='cardapio' element={<Menu />} />
+          </Route>
         </Routes>
       </Router>
     </main>
